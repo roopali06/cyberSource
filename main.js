b0bec148f9fa58f951f1a79f51aff5b3392be247
kuki06 <roopalipatil90@gmail.com>
2022-08-08 09:08:19 +0530

    changes in code

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
+const navbar = document.querySelector('.navbar');
+console.log(navbar);
 
 
+const changeNavcolor =()=>{
+       if (document.documentElement.scrollTop >= 300){
+              navbar.classList.add("changeColor1");
+              navbar.classList.remove("changeColor");
+       }else{
+              navbar.classList.add("changeColor");
+              navbar.classList.remove("changeColor1");
+       }
+}
+
+window.addEventListener('scroll',changeNavcolor);
+
+changeNavcolor();
